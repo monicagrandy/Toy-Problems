cabc818a1378f20dc9cbc35a0b6e3dea03cb4688
Monica Grandy <monica.grandy@gmail.com>
2016-05-23 10:40:27 -0700

    adds delete node in linked list

---
+++ Delete_Node_LinkedList.js
@@ -0,0 +1,10 @@
+// Write a function to delete a node (except the tail) in a singly linked list, given only access to that node.
+
+// Supposed the linked list is 1 -> 2 -> 3 -> 4 and you are given the third node with value 3, the linked list should become 1 -> 2 -> 4 after calling your function.
+
+var deleteNode = function(node) {
+    var temp = node
+    temp.val = node.next.val
+    temp.next = node.next.next
+    delete node
+};
