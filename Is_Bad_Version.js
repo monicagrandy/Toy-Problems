21ad9dfc9b8c16d5a6345275672f33819b8e6993
Monica Grandy <monica.grandy@gmail.com>
2016-05-25 18:26:06 -0700

    adds is bad version

---
+++ Is_Bad_Version.js
@@ -0,0 +1,28 @@
+// You are a product manager and currently leading a team to develop a new product. Unfortunately, the latest version of your product fails the quality check. Since each version is developed based on the previous version, all the versions after a bad version are also bad.
+
+// Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one, which causes all the following ones to be bad.
+
+// You are given an API bool isBadVersion(version) which will return whether version is bad. Implement a function to find the first bad version. You should minimize the number of calls to the API.
+
+var solution = function(isBadVersion) {
+    return function (n) {
+        if(n === 1){
+            return 1
+        }
+        var firstBad = 0
+        var min = 1
+        var max = n
+        while(min <= max){
+            var mid = Math.round(min + (max-min) / 2)
+            console.log(mid)
+            if(isBadVersion(mid)){
+                firstBad = mid
+                max = mid - 1
+            }
+            else {
+              min = mid + 1 
+            }
+        }
+        return firstBad
+    }
+};
